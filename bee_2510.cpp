063dfb4f06306f41e903a03c6e8e018a69f7fd27
Mateus Marttorelli <mateus.hmarttorelli@gmail.com>
2022-10-18 17:49:20 -0300

    Updating

---
+++ bee_2510.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    std::string vilao;
+
+    std::cin >> n;
+
+    while(n--)
+    {
+        std::cin >> vilao;
+        std::cout << "Y" << std::endl;
+    }
+
+    return 0;
+}
